cd18a74bac9cece351e03ed18cd43c259c0b6ebb
Littl3S3al <lauseal@gmail.com>
2020-04-28 13:21:09 +0200

    about page and scroll functionality and replace letter t

---
+++ scripts/letter-t.js
@@ -0,0 +1,23 @@
+// t char code is 116
+
+const h1 = document.querySelectorAll('h1');
+
+const fix = (text) => {
+    let updated = '';
+    for(i = 0; i < text.length; i++){
+        var code = text.charCodeAt(i);
+        if(code === 116){
+            updated = updated.concat('<span>t</span>');
+        } else {
+            updated = updated.concat(text[i]);
+        }
+    }
+    return (updated)
+};
+
+
+h1.forEach( heading => {
+    heading.innerHTML = fix(heading.innerText)
+})
+
+
